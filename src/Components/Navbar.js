5445abccba56bd663bcf8e30691884d043233000
Shafeeqsha1510 <shafeeqsha1510@gmail.com>
2023-11-06 15:21:44 +0530

    Navbar modified and added all links to projects

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,49 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import Home from './Home';
+
 function Navbar() {
-    const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
-    const toggleDropdown = () => {
-        setDropdownVisible(!isDropdownVisible);
-    };
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+  const closeDropdown =()=>{
+    setDropdownVisible(!isDropdownVisible)
+  }
 
-    return (
-        <div className='navbar-container'>
-            <a className='tag' href=''>MY REACT APP</a>
-            <a className='home' href=''>Home</a>
-            <a href="#" className='project' onClick={toggleDropdown}>
-                Project
-            </a>
-            {isDropdownVisible && (
-                <ul className="dropdown-content">
-                    <li><a href="">Home</a></li>
-                    <li><a href="#">Factorial</a></li>
-                    <li><a href="#">Word Count</a></li>
-                    <li><a href="#">Counter</a></li>
-
-                </ul>
-            )}
+  return (
+    <div className="navbar-container">
+      <Link to="/" className="tag">
+        MY REACT APP
+      </Link>
+      <Link to="/" className="home">
+        Home
+      </Link>
+      <a className="project" onClick={toggleDropdown} >
+        Project
+      </a>
+      {isDropdownVisible && (
+        <div class='link-container' >
+          <Link to="/fact" onClick={closeDropdown} className='links' >Factorial</Link>
+          <br />
+          <Link to="/count"onClick={closeDropdown}className='links' >Counter</Link>
+          <br />
+          <Link to="/input" onClick={closeDropdown }className='links'>InputField</Link>
+          <br />
+          <Link to="/searchword" onClick={closeDropdown}className='links'>Search Word</Link>
+          <br />
+          <Link to="/storeitems" onClick={closeDropdown}className='links'>Store Items</Link>
+          <br />
+          <Link to="/addcart" onClick={closeDropdown}className='links'>Add Cart</Link>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
+
+
+
+
