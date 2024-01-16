06fbb707d88394e5ce199c48c1d0fc322b8b1603
Shafeeqsha1510 <shafeeqsha1510@gmail.com>
2024-01-16 14:31:35 +0530

    Footer created

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import './Footer.css'
+  import React from 'react'
+  import './Footer.css'
+  import { Link } from 'react-router-dom'
 
-function Footer() {
-  return (
-    <div className='footer-container'>
-        Footer
-    </div>
-  )
-}
+  function Footer() {
+    return (
+      <div className='footer-container'>
+          <Link to='/all'  >All</Link>
+            <Link to='/men' >Mens</Link>
+            <Link to='/women' >Womens</Link>
+            <Link to='/adidas' >Adidas</Link>
+            <Link to='/puma' >Puma</Link>
+            <Link to='/nike' >Nike</Link>
+            <Link to='/crocs' >Crocs</Link>
+            <Link to='/skechers' >Skechers</Link>
+            <Link to='/newbalance' >New Balance</Link>
+            <Link to='/fila' >Fila</Link>
+      </div>
+    )
+  }
 
-export default Footer
+  export default Footer
